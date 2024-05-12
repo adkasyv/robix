d0b6d0fde995e3158b42fc607b7bc8e943cc9b08
isehou <zaku47shift@gmail.com>
2024-05-12 16:08:49 +0600

    upgraded UI, added adaptive on 70%

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let swiper = new Swiper(".mySwiper", {
+let promotionSwiper = new Swiper(".promotion-swiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   //   rewind: true,
@@ -6,4 +6,30 @@ let swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let svg = (document.createElementNS = "");
+
+document.getElementById("toggleList").addEventListener("click", function () {
+  let category = document.getElementById("category");
+  category.classList.toggle("show-list");
+  if (category.style.display === "none") {
+    this.textContent = "";
+  } else {
+    category.style.display = "block";
+  }
 });
