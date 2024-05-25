77cbb0e5f81d43c3e4e2ad078a8b3d53a139909f
isehou <zaku47shift@gmail.com>
2024-05-26 01:31:39 +0600

    upgraded heade UI and adaptive, upraded main-section UI and adaptive

--- script.js
+++ script.js
@@ -39,10 +39,10 @@ const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
-    menuBtn.classList.add("open");
+    menuBtn.classList.add("active");
     menuOpen = true;
   } else {
-    menuBtn.classList.remove("open");
+    menuBtn.classList.remove("active");
     menuOpen = false;
   }
 });
