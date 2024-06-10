3eaca16ea458209816f56ff3a4dc3f4306ca178a
isehou <zaku47shift@gmail.com>
2024-06-10 22:38:44 +0600

    fixed adaptive, upgraded UI

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // menu width-1024px
 
   let category = document.querySelector(".header__category");
-  const categoryItems = document.querySelectorAll(".header__category-item");
   const toggleBtn = document.getElementById("toggleList");
 
   toggleBtn.addEventListener("click", () => {
     for (let i = 1; i < categoryItems.length; i++) {
       categoryItems[i].classList.toggle("hidden");
     }
-
     if (category.style.display === "none") {
       category.classList.remove("active");
     } else {
@@ -23,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const menuBtn = document.querySelector(".menu-btn");
   const burgerPopup = document.getElementById("popup");
+  const categoryItems = document.getElementById("category-item");
 
   menuBtn.addEventListener("click", () => {
     if (!menuOpen) {
@@ -30,17 +29,23 @@ document.addEventListener("DOMContentLoaded", function () {
       category.classList.toggle("active-burger");
       menuBtn.classList.toggle("active");
       burgerPopup.classList.toggle("active");
+      categoryItems.classList.toggle("active");
+      categoryItems.classList.remove("hidden");
     } else {
       menuOpen = false;
       category.classList.remove("active-burger");
       menuBtn.classList.remove("active");
       burgerPopup.classList.remove("active");
+      categoryItems.classList.remove("active");
     }
   });
 
   function checkWindowSizeForBurgerBtn() {
-    if (window.innerWidth < 480) {
+    if (window.innerWidth > 768) {
       category.classList.remove("active-burger");
+      menuBtn.classList.remove("active");
+      burgerPopup.classList.remove("active");
+      categoryItems.classList.remove("active");
     } else {
     }
   }
@@ -49,11 +54,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // swiper
   let promotionSwiper = new Swiper(".promotion__swiper", {
-    slidesPerView: 3,
+    slidesPerView: 1,
+    slidesPerView: "auto",
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+    },
+    breakpoints: {
+      480: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
     },
   });
 
@@ -62,16 +79,14 @@ document.addEventListener("DOMContentLoaded", function () {
     slidesPerView: "auto",
     spaceBetween: 30,
     loop: true,
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
       480: {
         slidesPerView: 1,
       },
